eebc0763cc3de82f88cc8d639d2812afc466ee18
bbrandao1995 <bbrandao1995@gmail.com>
2018-05-02 21:35:52 -0300

    criado via script

---
+++ github.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+git config --global user.name "bbrandao1995"
+git config --global user.email bbrandao1995@gmail.com
+git clone https://github.com/bbrandao1995/LabSO2018.git
+git add *
+git commit
